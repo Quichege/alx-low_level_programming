2540009107f1c3bac134251a67cfe7440faa7fd9
Monicah Chege <monicahwangui28@gmail.com>
2023-03-21 12:58:19 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio>
+
+	/**
+	 * main -print_alphabet - print all alphabet in lowercase
+	 *
+	 * Return: 0
+	 */
+
+
+	void print_alphabet(void)
+	{
+		char letter;
+
+
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+
+
+		_putchar('\n');
+	}
+
+
+
