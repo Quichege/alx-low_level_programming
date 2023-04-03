f6dc121c43160daddd2027a0eb856b936fc05e26
Monicah Chege <monicahwangui28@gmail.com>
2023-04-03 10:06:50 -0700

    C-More pointers

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -2,7 +2,6 @@
 /**
  * print_chessboard - Entry point
  * @a: array
- *
  * Return: Always 0 (Success)
  */
 void print_chessboard(char (*a)[8])
@@ -12,7 +11,7 @@ void print_chessboard(char (*a)[8])
 
 	for (i = 0; i < 8; i++)
 	{
-		for (j = 0; i < 8; j++)
+		for (j = 0; j < 8; j++)
 			_putchar(a[i][j]);
 		_putchar('\n');
 	}
