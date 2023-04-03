24db446596e1788268353d2e505cd243ac56c93a
Monicah Chege <monicahwangui28@gmail.com>
2023-04-03 09:58:39 -0700

    C-More pointers

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -3,7 +3,6 @@
  * _strchr - Entry point
  * @s: input
  * @c: input
- *
  * Return: Always 0 (Success)
  */
 char *_strchr(char *s, char c)
@@ -12,7 +11,7 @@ char *_strchr(char *s, char c)
 
 	for (; s[i] >= '\0'; i++)
 	{
-		if ([i] == c)
+		if (s[i] == c)
 			return (&s[i]);
 	}
 	return (0);
